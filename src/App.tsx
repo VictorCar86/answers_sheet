101d46ec0ee9bbc025413dbec5a29e9d94ff12ab
Victor Carreno <vmanuelcar@hotmail.com>
2025-09-17 10:41:35 -0500

    build: enable allowJs in tsconfig and remove unused React import
    Enable JavaScript file support in TypeScript configuration to allow mixed codebase
    Remove unused React import as it's no longer needed with React 17+ JSX transform

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { DarkModeProvider } from './contexts/DarkModeContext';
 import DarkModeToggle from './components/DarkModeToggle';
 import ExamAnswerSheet from './components/ExamAnswerSheet';
